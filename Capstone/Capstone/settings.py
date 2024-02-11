3dd85b6c6ec416ed6c7825e813dc8c8337049305
isaacenobun <76211324+isaacenobun@users.noreply.github.com>
2024-02-11 10:18:44 +0100

    Changed database to mysql

--- Capstone/Capstone/settings.py
+++ Capstone/Capstone/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #added apps
     'capstone_app'
 ]
 
@@ -76,8 +78,15 @@ WSGI_APPLICATION = 'Capstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'LittleLemon',
+    'USER': 'root',
+    'PASSWORD': 'ayooz999',
+    'HOST': '127.0.0.1',
+    'PORT': '3306',
+    'OPTIONS': {
+    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    }
     }
 }
 
@@ -117,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = ['static']
 
 # Default primary key field type
